9ee6c99cbf83644bba5a92394439a184c688aa0e
Etex99 <emma99.kem@gmail.com>
2025-02-14 15:33:59 +0200

    completed steps of exercises 2.16 - 2.17 (phonebook)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import personsService from "./services/persons"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
+import Notification from './components/Notification'
 
 const App = () => {
   /* persons */
@@ -10,22 +11,29 @@ const App = () => {
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [searchFilter, setSearchFilter] = useState("")
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     personsService.getAll()
       .then(result => setPersons(result))
-      .catch(error => console.error(error))
+      .catch(error => {
+        setNotification({ message:"Could not fetch phonebook entries, please try again", type:"error" })
+        console.log(error)
+      })
   }, [])
 
   const handleRemovePerson = (personToRemove) => {
-    if (!confirm(`Delete ${personToRemove.name}?`)) {
-      return
+    if (confirm(`Delete ${personToRemove.name}?`)) {
+      personsService.remove(personToRemove.id)
+        .then(response => {
+          setPersons(persons.filter(i => i.id !== personToRemove.id))
+          notify(`${personToRemove.name} has been removed.`, "successful-action", 5000)
+        })
+        .catch(error => {
+          notify(`${personToRemove.name} has already been removed.`, "error", 5000)
+          console.log(error)
+        })
     }
-    personsService.remove(personToRemove.id)
-      .then(response => {
-        setPersons(persons.filter(i => i.id !== personToRemove.id))
-      })
-      .catch(error => console.error(error))
   }
 
   const onSubmitNewPerson = (event) => {
@@ -34,14 +42,22 @@ const App = () => {
     if (samePerson !== undefined) {
       if (confirm(`${newName} has already been added to the phonebook. Replace the old number with the new one?`)) {
         const modifiedPerson = {...samePerson, number: newNumber}
-        personsService.modify(modifiedPerson).then(result => {
-          setPersons(persons.map(i => i.id === result.id ? result : i))
-        })
+        personsService.modify(modifiedPerson)
+          .then(result => {
+            setPersons(persons.map(i => i.id === result.id ? result : i))
+            notify(`Number of ${modifiedPerson.name} has been modified`, "successful-action", 5000)
+          })
+          .catch(error => {
+            setPersons(persons.filter(i => i.id !== modifiedPerson.id))
+            console.log(error);
+            notify(`${modifiedPerson.name} cannot be modified as it does not exist`, "error", 5000)
+          })
       }
     } else {
       const newPerson = { name: newName, number: newNumber }
       personsService.create(newPerson).then(result => {
       setPersons(persons.concat(result))
+      notify(`${newName} has been added to the phonebook`, "successful-action", 5000)
     })
     }
     setNewName("")
@@ -52,8 +68,16 @@ const App = () => {
   const onSearchFilterChange = (event) => setSearchFilter(event.target.value)
   const personsToShow = persons.filter(i => i.name.toLowerCase().includes(searchFilter))
 
+  /* helpers */
+  const notify = (message, type, timeout) => {
+    setNotification({message, type})
+    setTimeout(() => setNotification(null), timeout)
+  }
+
   return (
     <div>
+      <Notification notification={notification}/>
+
       <h2>Phonebook</h2>
 
       <Filter searchFilter={searchFilter} onSearchFilterChange={onSearchFilterChange} />
